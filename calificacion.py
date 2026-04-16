672fac6a9e1fc9aeab19ba1fc07f2423bfa31c02
vicmorenog-byte <vic.morenog@duocuc.cl>
2026-04-16 16:20:16 -0400

    Calificaciones.py
    :)

---
+++ calificacion.py
@@ -0,0 +1,10 @@
+calificacion = int(input("ingrese su calificacion: "))
+
+if calificacion == 10 and 39:
+    print("reprobado")
+elif calificacion == 40 and 51:
+    print("aprobado")
+elif calificacion == 52 and 65:
+    print("excelente calificacion")
+elif calificacion == 66 and 70:
+    print("aprobado con honores")
