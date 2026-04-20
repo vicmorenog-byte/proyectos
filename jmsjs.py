b21e15c71e3f6ec8281b5ac6bc6fcba8daca5531
vicmorenog-byte <vic.morenog@duocuc.cl>
2026-04-20 15:38:05 -0400

    Create jmsjs.py

---
+++ jmsjs.py
@@ -0,0 +1,10 @@
+nombre = input("ingresa tu nombre: ")
+apellido = input("ingresa tu apellido: ")
+
+#CONCATENAR
+nombre_completo = nombre + " " + apellido
+
+print(F" nombre completo : {nombre_completo.upper()}")
+print(F" nombre completo : " len{nombre_completo})
+print(F"las iniciales son : {nombre{0.{apellido{0}}}}")
+
