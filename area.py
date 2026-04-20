0b35d5a5507d202fbce4560b7d18ed6a565bd73f
vicmorenog-byte <vic.morenog@duocuc.cl>
2026-04-20 15:02:45 -0400

    Create area.py
    :)

---
+++ area.py
@@ -0,0 +1,11 @@
+ancho = int(input("ingresa el primer numero"))
+largo = int(input("ingresa el segundo numero"))
+
+#calculo variables
+area = largo  * ancho
+#calcular el perimetro
+perimetro = (largo + ancho) * 2
+#mostrar resultados 
+print("el area del rectangulo es {area} y su perimetro es {perimetro}")
+
+
